5e3ae616b0ffee70bd52ccd6cc48c9bd3181e924
reFreshD2 <44308263+reFreshD2@users.noreply.github.com>
2019-05-10 11:55:01 +1000

    Maybe right

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -7,33 +7,44 @@ using namespace std;
 
 class Sort {
 public:
-  virtual void sorting (int* arr) = 0;
+    virtual void sorting (int* arr, int size) = 0;
 };
 
 class Merge : public Sort {
+    int left, right;
 public:
-    void sorting(int* arr,int l, int r) override;
+    void sorting(int* arr,int size) override;
+    void init(int size);
 };
 
-void Merge::sorting(int* arr, int l, int r) {
-    if (r == l)
+void Merge::sorting(int* arr, int size) {
+    if (right == left)
         return;
-    if (r - l == 1) {
-        if (arr[r] < arr[l])
-            swap(arr[r], arr[l]);
+    if (right - left == 1) {
+        if (arr[right] < arr[left])
+            swap(arr[right], arr[left]);
         return;
     }
-    int m = (r + l) / 2;
-    sorting(arr,l, m);
-    sorting(arr,m + 1, r);
+    int m = (right + left) / 2;
+    left = 0;
+    right = m;
+    sorting(arr,size);
+    left = m +1;
+    if (size%2 == 1){
+        right = m*2+1;
+    }
+    else{
+        right = m*2;
+    }
+    sorting(arr,size);
     int buf[max];
-    int xl = l;
+    int xl = left;
     int xr = m + 1;
     int cur = 0;
-    while (r - l + 1 != cur) {
+    while (right - left + 1 != cur) {
         if (xl > m)
             buf[cur++] = arr[xr++];
-        else if (xr > r)
+        else if (xr > right)
             buf[cur++] = arr[xl++];
         else if (arr[xl] > arr[xr])
             buf[cur++] = arr[xr++];
@@ -41,8 +52,13 @@ void Merge::sorting(int* arr, int l, int r) {
 
     }
     for (int i = 0; i < cur; i++)
-        arr[i + l] = buf[i];
+        arr[i + left] = buf[i];
 };
+
+void Merge::init(int size){
+    left = 0;
+    right = size;
+}
 
 int main() {
     Merge merge1;
@@ -53,7 +69,8 @@ int main() {
         cout << "Array[" << i << "] - ";
         cin >> arr[i];
     }
-    merge1.sorting(arr,0,n);
+    merge1.init(n);
+    merge1.sorting(arr,n);
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
     cout << endl;
