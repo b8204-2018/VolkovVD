dc734a79ed86b882a32a3fc4b88d86487686bdba
reFreshD2 <44308263+reFreshD2@users.noreply.github.com>
2019-06-14 16:05:06 +1000

    Without tests

--- OPZ.cpp
+++ OPZ.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include <stack>
+#include <string.h>
+#include <cstdlib>
 
 using namespace std;
 
-class Partition {
+class Parser {
     char *part;
     unsigned int size = 0;
 public:
 
-    void toPart (string equation){
-        stack <char> sign;
-        part = new char [equation.length()];
-        for (size_t i=0; i < equation.length(); i++) {
+    void toPart(string equation) {
+        stack<char> sign;
+        part = new char[equation.length()];
+        for (size_t i = 0; i < equation.length(); i++) {
             if (equation[i] >= 48 && equation[i] <= 57) { //цифры
                 part[size] = equation[i];
                 size++;
             }
-            if ((equation[i] == 40) || (equation[i] == 42) || (equation[i] == 43) || (equation[i] == 45) || (equation[i] == 47)) {  //открывающаяся скобка, + - * /
+            if ((equation[i] == 40) || (equation[i] == 42) || (equation[i] == 43) || (equation[i] == 45) ||
+                (equation[i] == 47)) {  //открывающаяся скобка, + - * /
                 sign.push(equation[i]);
             }
             if (equation[i] == 41) { //закрывающаяся скобка
                 while (sign.top() != 40) {
-                    part[size]=sign.top();
+                    part[size] = sign.top();
                     size++;
                     sign.pop();
                 }
@@ -35,54 +38,11 @@ public:
         }
     }
 
-    char* getPartition(){
+    char *getPartition() {
         return part;
     }
 
-    unsigned int getSize(){
-        return size;
-    }
-};
-
-class Polska{
-    char* polska;
-    unsigned int size=0;
-public:
-
-    void MakePolska(Partition partition){
-        int j=0;
-        stack <char> sign;
-        polska = new char [partition.getSize()];
-        char* part = partition.getPartition();
-        for (size_t i=0; i < partition.getSize(); i++){
-            if (part[i] >= 48 && part[i] <= 57) { //цифры
-                    polska[size] = part[i];
-                    size++;
-                }
-                if ((part[i] == 40) || (part[i] == 42) || (part[i] == 43) || (part[i] == 45) || (part[i] == 47)) {  //открывающаяся скобка, + - * /
-                    sign.push(part[i]);
-                }
-                if (part[i] == 41) { //закрывающаяся скобка
-                    while (sign.top() != 40) {
-                        polska[size] = sign.top();
-                        size++;
-                        sign.pop();
-                    }
-                    sign.pop();
-                }
-            }
-            while (!sign.empty()) {
-                polska[size] = sign.top();
-                size++;
-                sign.pop();
-            }
-    }
-
-    char* getPolska(){
-        return polska;
-    }
-
-    unsigned int getSize(){
+    unsigned int getSize() {
         return size;
     }
 };
@@ -91,16 +51,65 @@ class Calculator {
     int result;
 public:
 
-    int Calculated() {
-        stack <char> numbers;
-        for (size_t i=0; i < equation.length(); i++) {
-            if ((equation[i] == 42) || (equation[i] == 43) || (equation[i] == 45) || (equation[i] == 47)) {
-
+    int Calculated(Parser part) {
+        stack<int> numbers;
+        char *eq = new char[part.getSize()];
+        strcpy(eq, part.getPartition());
+        for (size_t i = 0; i < part.getSize(); i++) {
+            if (eq[i] >= 48 && eq[i] <= 57) {
+                int num = atoi(&eq[i]);
+                numbers.push(num);
+            }
+            if (eq[i] == 42) {
+                int res = numbers.top();
+                numbers.pop();
+                res = res * numbers.top();
+                numbers.pop();
+                numbers.push(res);
+            }
+            if (eq[i] == 43) {
+                int res = numbers.top();
+                numbers.pop();
+                res = res + numbers.top();
+                numbers.pop();
+                numbers.push(res);
+            }
+            if (eq[i] == 45) {
+                int res = numbers.top();
+                numbers.pop();
+                res = res - numbers.top();
+                numbers.pop();
+                numbers.push(res);
+            }
+            if (eq[i] == 47) {
+                int res = numbers.top();
+                numbers.pop();
+                res = res / numbers.top();
+                numbers.pop();
+                numbers.push(res);
             }
         }
+        result = numbers.top();
+        return result;
+    };
+};
+
+class Polska {
+    int result;
+public:
+    Polska(string equation) {
+        Parser parser;
+        parser.toPart(equation);
+        Calculator calculator;
+        result = calculator.Calculated(parser);
+    }
+
+    int getResult(){
+        return result;
     };
 };
 
 int main() {
-
+    Polska polska("((5+5)*(4-3))/2");
+    cout << polska.getResult();
 }
