c29341ee55ff57a209a1303058f32dc3e13cf0ce
reFreshD2 <44308263+reFreshD2@users.noreply.github.com>
2019-06-28 13:48:16 +1000

    Exceptions

--- OPZ.cpp
+++ OPZ.cpp
@@ -2,141 +2,145 @@
 #include <stack>
 #include <string.h>
 #include <cstdlib>
-#include <OPZ.h>
+#include "OPZ.h"
 
-using namespace std
+using namespace std;
 
-const int EMPTY = -1;
-const int ZERO = 0;
+const int OPENBRACKET = 40, CLOSEBRACKET = 41, MUL = 42, ADD = 43, DIV = 47, SUB = 45;
 
-class Parser {
-    char *part;
-    unsigned int size = 0;
-public:
-
-    void toPart(string equation) {
-        if (strlen(equation) < 3){
-            throw EMPTY;
+void Parser::toPart(string equation) {
+    if (equation.length() < 3) {
+        throw logic_error("Empty");
+    }
+    size_t open = 0, close = 0;
+    for (size_t i = 0; i < equation.length(); i++) {
+        if (equation[i] == OPENBRACKET) {
+            open++;
         }
-        catch(EMPTY);
-        size_t open = 0, close = 0;
-        for (size_t i = 0; i < strlen(equation); i++) {
-            if (equation[i] == 40) {
-                open++;
+        if (equation[i] == CLOSEBRACKET) {
+            close++;
+        }
+    }
+    if (open != close) {
+        throw logic_error("Open brackets <> Close brackets");
+    }
+    stack<char> sign;
+    char *number = new char[equation.length()];
+    int j = 0;
+    part = new char[equation.length()];
+    for (size_t i = 0; i < equation.length(); i++) {
+        while (isdigit(equation[i])) { //цифры
+            number[j] = equation[i];
+            j++;
+        }
+        if ((equation[i] == OPENBRACKET) || (equation[i] == MUL) || (equation[i] == ADD) || (equation[i] == SUB) ||
+            (equation[i] == DIV)) {  //открывающаяся скобка, + - * /
+            number[j + 1] = '\0';
+            for (size_t k = size, s = 0; j > 0; j--, s++, size++) {
+                part[k] = number[s];
             }
-            if (equation[i] == 41) {
-                close++;
-            }
+            sign.push(equation[i]);
         }
-        if (open != close) {
-            throw invalid_argument(equation);
-        }
-        catch(invalid_argument(equation));
-        stack<char> sign;
-        part = new char[equation.length()];
-        for (size_t i = 0; i < equation.length(); i++) {
-            if (equation[i] >= 48 && equation[i] <= 57) { //цифры
-                part[size] = equation[i];
+        if (equation[i] == CLOSEBRACKET) { //закрывающаяся скобка
+            while (sign.top() != OPENBRACKET) {
+                part[size] = sign.top();
                 size++;
-            }
-            if ((equation[i] == 40) || (equation[i] == 42) || (equation[i] == 43) || (equation[i] == 45) ||
-                (equation[i] == 47)) {  //открывающаяся скобка, + - * /
-                sign.push(equation[i]);
-            }
-            if (equation[i] == 41) { //закрывающаяся скобка
-                while (sign.top() != 40) {
-                    part[size] = sign.top();
-                    size++;
-                    sign.pop();
-                }
                 sign.pop();
             }
-        }
-        while (!sign.empty()) {
-            part[size] = sign.top();
             sign.pop();
-            size++;
         }
     }
-
-    char *getPartition() {
-        return part;
+    while (!sign.empty()) {
+        part[size] = sign.top();
+        sign.pop();
+        size++;
     }
+}
 
-    unsigned int getSize() {
-        return size;
-    }
-};
+char *Parser::getPartition() {
+    return part;
+}
 
-class Calculator {
-    int result;
-public:
+unsigned int Parser::getSize() {
+    return size;
+}
 
-    int Calculated(Parser part) {
-        stack<int> numbers;
-        char *eq = new char[part.getSize()];
-        strcpy(eq, part.getPartition());
-        for (size_t i = 0; i < part.getSize(); i++) {
-            if (eq[i] >= 48 && eq[i] <= 57) {
-                int num = atoi(&eq[i]);
-                numbers.push(num);
-            }
-            if (eq[i] == 42) {
-                int res = numbers.top();
-                numbers.pop();
-                res = res * numbers.top();
+int Calculator::Calculated(Parser part) {
+    stack<int> numbers;
+    int j = 0;
+    char *number = new char[part.getSize()];
+    char *eq = new char[part.getSize()];
+    strcpy(eq, part.getPartition());
+    for (size_t i = 0; i < part.getSize(); i++) {
+        while (isdigit(eq[i])) {
+            number[j] = eq[i];
+        }
+        number[j + 1] = '\0';
+        int num = atoi(number);
+        numbers.push(num);
+        j = 0;
+        if (eq[i] == MUL) {
+            int res = numbers.top();
+            numbers.pop();
+            res = res * numbers.top();
+            numbers.pop();
+            numbers.push(res);
+        }
+        if (eq[i] == ADD) {
+            int res = numbers.top();
+            numbers.pop();
+            res = res + numbers.top();
+            numbers.pop();
+            numbers.push(res);
+        }
+        if (eq[i] == SUB) {
+            int res = numbers.top();
+            numbers.pop();
+            res = res - numbers.top();
+            numbers.pop();
+            numbers.push(res);
+        }
+        if (eq[i] == DIV) {
+            int res = numbers.top();
+            numbers.pop();
+            if (res != 0) {
+                res = res / numbers.top();
                 numbers.pop();
                 numbers.push(res);
-            }
-            if (eq[i] == 43) {
-                int res = numbers.top();
-                numbers.pop();
-                res = res + numbers.top();
-                numbers.pop();
-                numbers.push(res);
-            }
-            if (eq[i] == 45) {
-                int res = numbers.top();
-                numbers.pop();
-                res = res - numbers.top();
-                numbers.pop();
-                numbers.push(res);
-            }
-            if (eq[i] == 47) {
-                int res = numbers.top();
-                numbers.pop();
-                if (res != 0) {
-                    res = res / numbers.top();
-                    numbers.pop();
-                    numbers.push(res);
-                }
-                else {
-                    throw (ZERO);
-                }
-                catch(ZERO);
+            } else {
+                throw invalid_argument("Division by zero");
             }
         }
-        result = numbers.top();
-        return result;
-    };
+    }
+    result = numbers.top();
+    return result;
 };
 
-class Polska {
-    int result;
-public:
-    Polska(string equation) {
+Polska::Polska(string equation) {
+    try {
         Parser parser;
-        parser.toPart(equation);
+    }
+    catch (logic_error e) {
+        cout << e.what();
+        return;
+    };
+    parser.toPart(equation);
+    try {
         Calculator calculator;
         result = calculator.Calculated(parser);
     }
-
-    int getResult() {
-        return result;
+    catch (invalid_argument e) {
+        cout << e.what();
+        return;
     };
+}
+
+int Polska::getResult() {
+    return result;
 };
 
 int main() {
     Polska polska("((5+5)*(4-3))/2");
     cout << polska.getResult();
+    return 0;
 }
